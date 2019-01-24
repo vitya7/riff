250997c747fe88c8153ac39acbe70915ece23596
vitya7 <31449667+vitya7@users.noreply.github.com>
2019-01-25 00:58:14 +0400

    tab fix in fmt_chunk

--- include/fmt_chunk.hpp
+++ include/fmt_chunk.hpp
@@ -12,10 +12,10 @@ namespace riff
     {
         header  head;
         word_t  compression_code;
-		word_t  num_of_channels;
-		dword_t sample_rate;
-		dword_t avg_bytes_per_sec;
-		word_t  block_align;
-		word_t  bits_per_sample;
+        word_t  num_of_channels;
+        dword_t sample_rate;
+        dword_t avg_bytes_per_sec;
+        word_t  block_align;
+        word_t  bits_per_sample;
     };
 }
