9761ebb267e608de45cd2aa82dd6aa6fb9c39d41
vitya7 <31449667+vitya7@users.noreply.github.com>
2019-02-01 16:54:47 +0400

    +header for library

---
+++ include/riff.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "riff_types.hpp"
+#include "riff_const.hpp"
+
+#include "header.hpp"
+
+#include "riff_stream.hpp"
+
+#include "header_iterator.hpp"
+#include "wave_data_iterator.hpp"
+
+#include "fmt_chunk.hpp"
+#include "multi_channel_sample.hpp"
